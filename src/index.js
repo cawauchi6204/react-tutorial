011a2e8812a97c74279e1967657c20a8f229e08b
cawauchi6204 <cawauchi6204@gmail.com>
2020-06-29 11:34:49 +0900

    ゲーム勝者の判定まで進んだ（勝者判定の文の理解が浅い）

--- src/index.js
+++ src/index.js
@@ -32,25 +32,34 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squeares: Array(9).fill(null),
-            xIsNext:true
+            squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
         return <Square
-            value={this.state.squeares[i]}
+            value={this.state.squares[i]}
             onClick={() => this.handleClick(i)}
         />;
     }
 
     render() {
-        const status = 'Next player: X';
-
+        const winner =
+            calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner:' + winner;
+        } else {
+            status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -88,6 +97,30 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    //勝利条件
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 6, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        //linesの数だけfor文を回して[a,b,c]に代入している
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+    //この文の理解が浅い
 }
 
 // ========================================
